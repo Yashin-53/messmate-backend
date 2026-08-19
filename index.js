a3537a80e273387b80b55e25635fe268014e4adc
Yashin Ansari <yashinansari2002@gmail.com>
2026-08-20 01:07:17 +0530

    added frontend deployed link in index.js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: [
+        "http://localhost:3001",
+        "https://messmate-frontend.netlify.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
